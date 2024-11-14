1064182080235ebe12492320e55a4f5fa9fb514e
unknown <shivamanand8969@gmail.com>
2024-11-15 00:07:12 +0530

    added build

--- src/api/tripApi.jsx
+++ src/api/tripApi.jsx
@@ -1,5 +1,7 @@
 import Axios from "axios";
 
 export default Axios.create({
-  baseURL: "https://sapthapadhi.bloomitsolutions.co.in",
+  // baseURL: "https://sapthapadhi.bloomitsolutions.co.in",
+baseURL: "https://sapthapadhimatrimony.in/backend",
 });
+// baseURL: "https://sapthapadhimatrimony.in/backend",
